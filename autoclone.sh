8a29f012333d13f35d4f1417cb17a653482a7447
l1nux-th1ngz <137152321+l1nux-th1ngz@users.noreply.github.com>
2023-12-31 16:31:49 -0700

    Create autoclone.sh

---
+++ autoclone.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+while true
+do
+    echo "1. Clone the entire repository"
+    echo "2. Clone the entire repository"
+    echo "3. Clone the entire repository"
+    echo "4. Clone the entire repository"
+    echo "5. Clone the entire repository"
+    echo "Please wait while your GITS clone:"
+
+    read CHOICE
+
+    echo "Enter the Git links:"
+    read REPO_URL
+
+    case $CHOICE in
+      1)
+        # Clone the entire repository
+        git clone $REPO_URL
+        ;;
+      2)
+        # Clone the entire repository
+        git clone $REPO_URL
+         ;;
+      3)
+        # Clone the entire repository
+        git clone $REPO_URL
+         ;;
+     4)
+     # Clone the entire repository
+        git clone $REPO_URL
+         ;;
+    5)
+    # Clone the entire repository
+        git clone $REPO_URL
+         ;;
+    esac
+
+    echo "Your GITS are done cloning."
+    clear
+done
